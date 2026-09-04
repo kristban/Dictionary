d8df206d056ae5be2dadb20432a87fc7856eaa29
kristban <kristinbannasch@hotmail.com>
2026-09-04 06:50:31 +0100

    Add shih-tzu sticker to header and header styles
    Add new shih-tzu sticker asset and include it in the App header. Update App.jsx to render the image and add CSS rules (.title and header) to style the image and header typography (font, size, color, alignment, margin).

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img class="title" src="./src/assets/shih-tzu-sticker2.png" alt="pepper" />
         Dictionary
       </header>
     </div>
